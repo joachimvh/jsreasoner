d6b3452549e58b55d50044b76b19b59f3e3769a8
Joachim Van Herwegen <joachimvh@gmail.com>
2017-10-24 10:43:26 +0200

    Expose components through index.js

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 
-let T = require('./terms/Terms');
-let ForwardReasoner = require('./reasoning/ForwardReasoner');
-let BackwardReasoner = require('./reasoning/BackwardReasoner');
-let Parser = require('./parsing/N3Parser');
-let BackParser = require('./parsing/TermParser');
-let fs = require('fs');
+const Terms = require('./terms/Terms');
+const ForwardReasoner = require('./reasoning/ForwardReasoner');
+// const BackwardReasoner = require('./reasoning/BackwardReasoner'); // not exposing this until fixed
+const N3Parser = require('./parsing/N3Parser');
+const TermParser = require('./parsing/TermParser');
 
-// let parser = new Parser();
-// let terms = parser.toTerms(':a :k :o. :a :b :c. {?x :b :c}=>{{?x :k ?y}=>{:p :p ?y}}.');
-// let goals = parser.toTerms(':p :p :o.');
-// console.log('FORWARD REASONING');
-// console.log(new ForwardReasoner().reason(terms).map(({data, evidence}) => [data, ...evidence].join('\n    ')).join('\n'));
-// console.log('BACKWARD REASONING');
-// console.log(new BackwardReasoner().reason(goals, terms).map(({data, evidence}) => [data, ...evidence].join('\n    ')).join('\n'));
-
-let parser = new Parser();
-let terms = parser.toTerms('{?everyone :says {_:x a :Unicorn}}=>{ :z :z :z }.');
-let result = [].concat(...terms.map(t => t.toSNF()));
-console.log(result.join('\n'));
-result = BackParser.fromSNF(result);
-console.log(result.join('\n'));
+module.exports = {
+  ForwardReasoner,
+  N3Parser,
+  TermParser,
+  Terms
+};
---
+++ test.js
@@ -0,0 +1,22 @@
+
+let T = require('./terms/Terms');
+let ForwardReasoner = require('./reasoning/ForwardReasoner');
+let BackwardReasoner = require('./reasoning/BackwardReasoner');
+let Parser = require('./parsing/N3Parser');
+let BackParser = require('./parsing/TermParser');
+let fs = require('fs');
+
+// let parser = new Parser();
+// let terms = parser.toTerms(':a :k :o. :a :b :c. {?x :b :c}=>{{?x :k ?y}=>{:p :p ?y}}.');
+// let goals = parser.toTerms(':p :p :o.');
+// console.log('FORWARD REASONING');
+// console.log(new ForwardReasoner().reason(terms).map(({data, evidence}) => [data, ...evidence].join('\n    ')).join('\n'));
+// console.log('BACKWARD REASONING');
+// console.log(new BackwardReasoner().reason(goals, terms).map(({data, evidence}) => [data, ...evidence].join('\n    ')).join('\n'));
+
+let parser = new Parser();
+let terms = parser.toTerms('{?everyone :says {_:x a :Unicorn}}=>{ :z :z :z }.');
+let result = [].concat(...terms.map(t => t.toSNF()));
+console.log(result.join('\n'));
+result = BackParser.fromSNF(result);
+console.log(result.join('\n'));
