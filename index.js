5b01b87ef6c80e597014290a6d5123ec1101297f
Joachim Van Herwegen <joachimvh@gmail.com>
2018-04-19 11:28:56 +0200

    Expose BackwardReasoner

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 
 const Terms = require('./terms/Terms');
 const ForwardReasoner = require('./reasoning/ForwardReasoner');
-// const BackwardReasoner = require('./reasoning/BackwardReasoner'); // not exposing this until fixed
+const BackwardReasoner = require('./reasoning/BackwardReasoner');
 const N3Parser = require('./parsing/N3Parser');
 const TermParser = require('./parsing/TermParser');
 
 module.exports = {
   ForwardReasoner,
+  BackwardReasoner,
   N3Parser,
   TermParser,
   Terms
